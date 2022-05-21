a3dd845dcf1a16d3b765ceb16ebf70ac70624e74
Enofua Divine <divineenofua@gmail.com>
2022-05-21 13:32:14 -0700

    new commit

--- script.js
+++ script.js
@@ -6,4 +6,29 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times')
     navbar.classList.toggle('active')
 }
+// window.onscroll = () =>{
+//     menu.classList.toggle('fa-times')
+//     navbar.classList.toggle('active')
+// }
 
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+} 
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+} 
+
+// slider
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
