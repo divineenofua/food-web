d593b8a70b66e8ff121c58c1d4e8095a7a818b9e
Enofua Divine <divineenofua@gmail.com>
2022-05-29 17:26:51 -0700

    my latest commit

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+  if(this.scrollY > 500){
+    $('.scroll-up-btn').addClass("show");
+}else{
+    $('.scroll-up-btn').removeClass("show");
+}
+});
+
+ // slide-up script
+ $('.scroll-up-btn').click(function(){
+  $('html').animate({scrollTop: 0});
+});
+
 // menu bar
 var menu = document.querySelector("#menubars");
 var navbar = document.querySelector(".navbar");
@@ -10,6 +24,9 @@ menu.onclick = () =>{
 //     menu.classList.toggle('fa-times')
 //     navbar.classList.toggle('active')
 // }
+
+
+
 
 document.querySelector('#search-icon').onclick = () =>{
     document.querySelector('#search-form').classList.toggle('active');
@@ -32,3 +49,53 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true,
   });
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+  });
+
+  // typing animation script
+
+var typed = new Typed(".typing", {
+  strings: ["we'll statisfy your love for food.. our dishes will leave you in awe and keep you coming for more."],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
+
+var typed = new Typed(".typing2", {
+  strings: ["we look forward to taking your orders."],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
+});
+
+
+
+
+
+
+
+  // document.addEventListener("DOMContentLoaded", function () {
+  //   let letter = 0;
+  //   const text = "spuring your love for food ...our dishes will leave yoou enarmored.";
+  //   function typeText() {
+  //     if(letter < text.length){
+  //       document.getElementById("type-js").innerHTML += text.charAt(letter);
+  //       letter++;
+  //       let speed = math.floor(math.random() * 150) + 50;
+  //       setTimeout(typeText, speed);
+  //     }
+  //   }
+  //   typeText();
+  // });
+
